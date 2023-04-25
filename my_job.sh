c0a9ecdebca85f9230254e8a139d57bee2408acd
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-25 13:55:19 -0400

    new

--- my_job.sh
+++ my_job.sh
@@ -8,7 +8,14 @@
 #BSUB -N
 
 module purge
-module load cuda/9.0
+
+module load utilities/multi
+module load readline/7.0
+module load gcc/10.2.0
+module load cuda/11.5.0
+module load python/anaconda/4.6/miniconda/3.7
+
+
 source /cluster/home/quintep/myproject_env/bin/activate
 echo ">>> Installing Requirements";
 pip install -r requirements.txt
