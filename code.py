0b4721459811b27be1648100381d713e28c2c39e
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-25 09:55:12 -0400

    save

--- code.py
+++ code.py
@@ -1,4 +1,9 @@
 import numpy as np
+import random
+import matplotlib.pyplot as plt
 a = 3+5
 b = 6*a^3
+c = np.random.random((256,256))
 print('old->', a, 'new->' ,b)
+plt.imshow(c)
+plt.savefig('random.png')
