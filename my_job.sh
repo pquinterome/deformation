95d542300f9155f252e644db6a2d86a09ca09111
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-25 15:04:14 -0400

    all

--- my_job.sh
+++ my_job.sh
@@ -2,20 +2,13 @@
 #BSUB -n 24                     # 24 cores
 #BSUB -W 8:00                   # 8-hour run-time
 #BSUB -R "rusage[mem=4000]"     # 4000 MB per core
-#BSUB -J analysis1
+#BSUB -J XCATphantom
 #BSUB -o model.out
 #BSUB -e model.err
 #BSUB -N
 
 module purge
-
-module load utilities/multi
-module load readline/7.0
-module load gcc/10.2.0
-module load cuda/11.5.0
-module load python/anaconda/4.6/miniconda/3.7
-
-
+echo ">>> Open environment";
 source /cluster/home/quintep/myproject_env/bin/activate
 echo ">>> Installing Requirements";
 pip install -r requirements.txt
