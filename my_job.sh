8bf138777832081fb14b0f190d55adaa59a1ff1f
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-25 15:33:15 -0400

    all

--- my_job.sh
+++ my_job.sh
@@ -6,13 +6,9 @@
 #BSUB -o model.out
 #BSUB -e model.err
 #BSUB -N 4
-#BSUB -p gpu05,gpu
-#BSUB --gres=gpu:1
-#BSUB --time=20:00:00
+
 
 module purge
-
-pip install -r requirements.txt
 echo ">>> Open environment";
 source /cluster/home/quintep/myproject_env/bin/activate
 echo ">>> Installing Requirements";
