bb38c1aa13788055e594b5e6f73fc7453323b91b
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-25 11:53:57 -0400

    por

--- my_job.sh
+++ my_job.sh
@@ -3,12 +3,12 @@
 #BSUB -W 8:00                   # 8-hour run-time
 #BSUB -R "rusage[mem=4000]"     # 4000 MB per core
 #BSUB -J analysis1
-#BSUB -o analysis1.out
-#BSUB -e analysis1.err
+#BSUB -o model.out
+#BSUB -e model.err
 #BSUB -N
 
 module purge
 module load cuda/9.0
 source myproject_env/bin/activate
-python deformation/code.py
+python code.py
 conda deactivate
