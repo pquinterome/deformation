3843c342acb9d7ea46c8e29ce600445d48c43450
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-04-26 11:08:24 -0400

    asdews

--- code.py
+++ code.py
@@ -1,6 +1,27 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
+
+from cProfile import label
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+import pandas as pd
+from pyparsing import alphas
+import seaborn as sns
+import tensorflow as tf
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, MaxPool2D, Flatten, Dropout, GlobalMaxPooling2D
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
+from scipy.stats import shapiro
+from scipy.stats import spearmanr
+from scipy.stats import pearsonr
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+from sklearn.metrics import plot_roc_curve, auc, precision_score, recall_score, f1_score, roc_curve
+from numpy import interp
+
 a = 3+5
 b = 6*a^3
 c = np.random.random((256,256))
--- my_job.sh
+++ my_job.sh
@@ -3,8 +3,8 @@
 #BSUB -W 8:00                   # 8-hour run-time
 #BSUB -R "rusage[mem=4000]"     # 4000 MB per core
 #BSUB -J phantom
-#BSUB -o model.out
-#BSUB -e model.err
+#BSUB -o %.out
+#BSUB -e %.err
 #BSUB -N
 
 module purge
@@ -13,4 +13,3 @@ echo ">>> Installing Requirements";
 conda run -n xcat_phantom pip install -r /cluster/home/quintep/deformation/requirements.txt
 echo ">>> Running Code";
 python /cluster/home/quintep/deformation/code.py
-deactivatecd
