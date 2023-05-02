714a2936fe2b7056c5fda02373e20d6d5d87e748
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-05-02 12:26:15 -0400

    test_1

--- xcat_code.py
+++ xcat_code.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 import pandas as pd
+import SimpleITK as sitk
 from pyparsing import alphas
 import seaborn as sns
 import sklearn
@@ -27,7 +28,7 @@ ct_4D = np.load('inputs/ct_4D.npy', allow_pickle=True)
 dvf = np.load('inputs/dvf.npy', allow_pickle=True)
 print('DVF shape', dvf.shape)
 a_dvf = dvf.mean(axis=0)
-#
+###### PCA #################
 pca_3 = PCA(n_components=3, random_state=2046)
 flat = np.reshape(dvf, (9, 70*256*256*3))
 pca_3.fit(flat)
@@ -35,8 +36,33 @@ eigen_vect = pca_3.components_
 eigen_val = pca_3.explained_variance_
 print('Eigen Vectors Shape', eigen_vect.shape)
 print('Eigen Values Shape', eigen_val.shape)
+##############################
+sampl = np.load('sample.npy')
+vect = eigen_vect.reshape(3, 70, 256, 256, 3)
+d = []
+l = []
+for r in range(1000):   
+    val = sampl[r]/10000
+    l.append(val)
+    vectors = a_dvf + sum(np.array([vect[i]*val[i] for i in range(len(sampl[8]))]))
+    displacement_image = sitk.GetImageFromArray(vectors, isVector=True)
+    # Set the displacement image's origin and spacing to relevant values
+    displacement_image.SetOrigin((0,0,0))
+    displacement_image.SetSpacing((1,1,1))
+    tx = sitk.DisplacementFieldTransform(displacement_image)
+    moving_image = sitk.GetImageFromArray(np.ascontiguousarray(xcat_ct[0][::-1,:,:]))
+    #sitk.WriteImage(fixed_image, os.path.join(OUTPUT_DIR, f"mr/fixed_image{i}.mha"))
+    syn_deform = sitk.Resample(moving_image, tx)
+    syn_deform = sitk.GetArrayViewFromImage(syn_deform) 
+    syn_deform = syn_deform[::-1,:,:]   
+    d.append(syn_deform)
+ll = np.array(l)
+dd = np.array(d)
 #
-#
+np.save('eigen_val.npy', ll)
+np.save('dataset_imgs.npy', dd)
+print('eigenvalues shape', ll.shape)
+print('dataset_images', dd.shape)
 print('All is going ok')
 a = 3+5
 b = 6*a^3
