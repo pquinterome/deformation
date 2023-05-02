85140ea10233aae63b6d90d8d1022a2ffb7c8b6a
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-05-02 08:55:53 -0400

    push

--- xcat_code.py
+++ xcat_code.py
@@ -21,6 +21,21 @@ from scipy.stats import pearsonr
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from sklearn.metrics import RocCurveDisplay, auc, precision_score, recall_score, f1_score, roc_curve
 from numpy import interp
+#
+ct_4D = np.load('inputs/ct_4D.npy')
+dvf = np.load('inputs/dvf.npy')
+a_dvf = dvf.mean(axis=0)
+from sklearn.decomposition import PCA
+pca_3 = PCA(n_components=3, random_state=2046)
+flat = np.reshape(dvf, (9, 70*256*256*3))
+pca_3.fit(flat)
+eigen_vect = pca_3.components_
+eigen_val = pca_3.explained_variance_
+print('Eigen Vectors Shape', eigen_vect.shape)
+print('Eigen Values Shape', eigen_val.shape)
+#
+#
+print('All is going ok')
 a = 3+5
 b = 6*a^3
 c = np.random.random((256,256))
