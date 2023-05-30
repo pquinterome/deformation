6c394a3488a30a6a5ffa4eb4b005e90462693b64
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-05-30 08:54:52 -0400

    try_1

--- my_job.sh
+++ my_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #BSUB -n 120                     # 24 cores
 #BSUB -W 80:00                   # 80-hour run-time
-#BSUB -R "rusage[mem=400000]"     # 4000 MB per core
+#BSUB -R "rusage[mem=240GB]"     # 4000 MB per core
 #BSUB -J phantom
 #BSUB -o model.out
 #BSUB -e model.err
