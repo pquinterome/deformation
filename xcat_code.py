958cbc7040fdba09754e36dbc45ab12b96510a06
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-07-26 09:38:50 -0400

    correct

--- xcat_code.py
+++ xcat_code.py
@@ -41,8 +41,8 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 #########Inputs and Outputs##########
 y= np.load('inputs/sample.npy',allow_pickle=True)
 y = y[:,0]
-y1 = y[:500]
-y2 = y[-500:]
+y1 = y[:50]
+y2 = y[-50:]
 y = np.concatenate((y1,y2))
 y = np.array([-y[i]/y.min() if y[i]<0 else y[i]/y.max() for i in range(len(y))])
 print('Output Size', y.shape)
