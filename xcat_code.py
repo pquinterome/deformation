cb621626527c1d3c62e46cb45c82aeb6297ca8ba
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-07-26 09:35:38 -0400

    try 2

--- xcat_code.py
+++ xcat_code.py
@@ -41,16 +41,16 @@ print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 #########Inputs and Outputs##########
 y= np.load('inputs/sample.npy',allow_pickle=True)
 y = y[:,0]
-y1 = y[:100]
-y2 = y[-100:]
+y1 = y[:500]
+y2 = y[-500:]
 y = np.concatenate((y1,y2))
 y = np.array([-y[i]/y.min() if y[i]<0 else y[i]/y.max() for i in range(len(y))])
 print('Output Size', y.shape)
 
 mri_1= np.load('inputs/images_1.npy', allow_pickle=True)
-mri_1 = mri_1[:100]
+mri_1 = mri_1[:50]
 mri_2 = np.load('inputs/images_2.npy',allow_pickle=True)
-mri_2 = mri_2[-100:]
+mri_2 = mri_2[-50:]
 mri = np.concatenate((mri_1, mri_2))
 #mri = np.array([mri[i][:,125,:] for i in range(len(mri))])
 print('MRI_size', mri.shape)
@@ -174,8 +174,8 @@ xw = mri
 X_train, X_test, y_train, y_test = train_test_split(xw, y, test_size=0.2) #random_state=1
 print(X_train.shape)
 print(X_test.shape)
-X_train = X_train.reshape(160, 80, 256, 256, 1)
-X_test = X_test.reshape(40, 80, 256, 256, 1)
+X_train = X_train.reshape(80, 80, 256, 256, 1)
+X_test = X_test.reshape(20, 80, 256, 256, 1)
 print(X_train.shape)
 print(X_test.shape)
 #
@@ -295,8 +295,8 @@ for model in models:
     X_train, X_test, y_train, y_test = train_test_split(xw, y, test_size=0.2) #random_state=1
     print(X_train.shape)
     print(X_test.shape)
-    X_train = X_train.reshape(160, 80, 256, 256, 1)
-    X_test = X_test.reshape(40, 80, 256, 256, 1)   
+    X_train = X_train.reshape(80, 80, 256, 256, 1)
+    X_test = X_test.reshape(20, 80, 256, 256, 1)   
     #model1.fit(x=X_train, y= y_train, validation_data= (X_test, y_test), epochs=100, callbacks=[early_stop], verbose=2, batch_size=5)
     pred = model1.predict(X_test)
     s = r2_score(y_test, pred)
