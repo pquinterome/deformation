1b101d27eb50857a58dc3d97e62d217066aa87e6
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-07-26 10:59:13 -0400

    terw

--- my_job.sh
+++ my_job.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#BSUB -n 120                     # 24 cores
+#BSUB -gpu "num=1:mode=exclusive_process:mps=no:j_exclusive=yes"
+#BSUB -W 80:00                   # 80-hour run-time
+#BSUB -R "rusage[mem=240GB]"     # 4000 MB per core
+#BSUS -q research
+#BSUB -J phantom
+#BSUB -o model.out
+#BSUB -e model.err
+#BSUB -N
+
+
+#module purge
+#module avail
+#module load cuda
+
+#echo ">>> Installing Requirements";
+#conda run -n xcat pip install -r /cluster/home/quintep/deformation/requirements.txt
 echo ">>> Running Code";
 python /cluster/home/quintep/deformation/xcat_code.py
 echo ">>> End";
