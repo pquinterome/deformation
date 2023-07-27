489bf29affc1cb659140e3cdfcbde987ef2566c2
pquinterome <33006816+pquinterome@users.noreply.github.com>
2023-07-27 10:16:28 -0400

    clean

--- xcat_code.py
+++ xcat_code.py
@@ -32,7 +32,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping
 import h5py
 
-#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #tf.config.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
