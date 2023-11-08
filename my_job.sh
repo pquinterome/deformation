2ee7be38a3bd6aca485f4a5dab0030601f62d1fe
Paulo_Quintero <33006816+pquinterome@users.noreply.github.com>
2023-11-08 10:41:07 -0500

    gan

--- my_job.sh
+++ my_job.sh
@@ -18,5 +18,5 @@
 #echo ">>> Installing Requirements";
 #conda run -n xcat pip install -r /cluster/home/quintep/deformation/requirements.txt
 echo ">>> Running Code";
-python /cluster/home/quintep/deformation/xcat_code.py
+python /cluster/home/quintep/deformation/gan.py
 echo ">>> End";
