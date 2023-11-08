fe2455b4bdeb78a2de25e26b21119a9720b128ea
Paulo_Quintero <33006816+pquinterome@users.noreply.github.com>
2023-11-08 16:16:33 -0500

    gpu222

--- gan.py
+++ gan.py
@@ -199,103 +199,105 @@ from tensorflow.keras.optimizers.legacy import Adam
 # Binary cross entropy is going to be the loss for both 
 from tensorflow.keras.losses import BinaryCrossentropy
 #
-g_opt = Adam(learning_rate=0.0001) 
-d_opt = Adam(learning_rate=0.00001) 
-g_loss = BinaryCrossentropy()
-d_loss = BinaryCrossentropy()
-# Importing the base model class to subclass our training step 
-from tensorflow.keras.models import Model
-#
-#
-#
-class FashionGAN(Model): 
-    def __init__(self, generator, discriminator, *args, **kwargs):
-        # Pass through args and kwargs to base class 
-        super().__init__(*args, **kwargs)
-        
-        # Create attributes for gen and disc
-        self.generator = generator 
-        self.discriminator = discriminator 
-        
-    def compile(self, g_opt, d_opt, g_loss, d_loss, *args, **kwargs): 
-        # Compile with base class
-        super().compile(*args, **kwargs)
-        
-        # Create attributes for losses and optimizers
-        self.g_opt = g_opt
-        self.d_opt = d_opt
-        self.g_loss = g_loss
-        self.d_loss = d_loss 
-
-    def train_step(self, batch):
-        # Get the data 
-        real_images = batch
-        fake_images = self.generator(tf.random.normal((100, 10, 1)), training=False)
-        
-        # Train the discriminator
-        with tf.GradientTape() as d_tape: 
-            # Pass the real and fake images to the discriminator model
-            yhat_real = self.discriminator(real_images, training=True) 
-            yhat_fake = self.discriminator(fake_images, training=True)
-            yhat_realfake = tf.concat([yhat_real, yhat_fake], axis=0)
-            
-            # Create labels for real and fakes images
-            y_realfake = tf.concat([tf.zeros_like(yhat_real), tf.ones_like(yhat_fake)], axis=0)
-            
-            # Add some noise to the TRUE outputs
-            noise_real = 0.15*tf.random.uniform(tf.shape(yhat_real))
-            noise_fake = -0.15*tf.random.uniform(tf.shape(yhat_fake))
-            y_realfake += tf.concat([noise_real, noise_fake], axis=0)
-            
-            # Calculate loss - BINARYCROSS 
-            total_d_loss = self.d_loss(y_realfake, yhat_realfake)
-            
-        # Apply backpropagation - nn learn 
-        dgrad = d_tape.gradient(total_d_loss, self.discriminator.trainable_variables) 
-        self.d_opt.apply_gradients(zip(dgrad, self.discriminator.trainable_variables))
-        
-        # Train the generator 
-        with tf.GradientTape() as g_tape: 
-            # Generate some new images
-            gen_images = self.generator(tf.random.normal((100,10,1)), training=True)
-                                        
-            # Create the predicted labels
-            predicted_labels = self.discriminator(gen_images, training=False)
-                                        
-            # Calculate loss - trick to training to fake out the discriminator
-            total_g_loss = self.g_loss(tf.zeros_like(predicted_labels), predicted_labels) 
-            
-        # Apply backprop
-        ggrad = g_tape.gradient(total_g_loss, self.generator.trainable_variables)
-        self.g_opt.apply_gradients(zip(ggrad, self.generator.trainable_variables))
-        
-        return {"d_loss":total_d_loss, "g_loss":total_g_loss}
-# Create instance of subclassed model
-fashgan = FashionGAN(generator, discriminator)
-# Compile the model
-fashgan.compile(g_opt, d_opt, g_loss, d_loss)
-import os
-from tensorflow.keras.preprocessing.image import array_to_img
-from tensorflow.keras.callbacks import Callback
-class ModelMonitor(Callback):
-    def __init__(self, num_img=3, latent_dim=10):
-        self.num_img = num_img
-        self.latent_dim = latent_dim
-
-    def on_epoch_end(self, epoch, logs=None):
-        random_latent_vectors = tf.random.uniform((self.num_img, self.latent_dim,1))
-        generated_images = self.model.generator(random_latent_vectors)
-        generated_images *= 255
-        generated_images.numpy()
-        for i in range(self.num_img):
-            img = array_to_img(generated_images[i])
-            img.save(os.path.join('images', f'generated_img_{epoch}_{i}.png'))
-#
-#
-# Recommend 2000 epochs
-print('ready to train')
 strategy = tf.distribute.MirroredStrategy(devices=["/gpu:0", "/gpu:1"])
 with strategy.scope():
+    g_opt = Adam(learning_rate=0.0001) 
+    d_opt = Adam(learning_rate=0.00001) 
+    g_loss = BinaryCrossentropy()
+    d_loss = BinaryCrossentropy()
+    # Importing the base model class to subclass our training step 
+    from tensorflow.keras.models import Model
+    #
+    #
+    #
+    class FashionGAN(Model): 
+        def __init__(self, generator, discriminator, *args, **kwargs):
+            # Pass through args and kwargs to base class 
+            super().__init__(*args, **kwargs)
+            
+            # Create attributes for gen and disc
+            self.generator = generator 
+            self.discriminator = discriminator 
+            
+        def compile(self, g_opt, d_opt, g_loss, d_loss, *args, **kwargs): 
+            # Compile with base class
+            super().compile(*args, **kwargs)
+            
+            # Create attributes for losses and optimizers
+            self.g_opt = g_opt
+            self.d_opt = d_opt
+            self.g_loss = g_loss
+            self.d_loss = d_loss 
+
+        def train_step(self, batch):
+            # Get the data 
+            real_images = batch
+            fake_images = self.generator(tf.random.normal((100, 10, 1)), training=False)
+            
+            # Train the discriminator
+            with tf.GradientTape() as d_tape: 
+                # Pass the real and fake images to the discriminator model
+                yhat_real = self.discriminator(real_images, training=True) 
+                yhat_fake = self.discriminator(fake_images, training=True)
+                yhat_realfake = tf.concat([yhat_real, yhat_fake], axis=0)
+                
+                # Create labels for real and fakes images
+                y_realfake = tf.concat([tf.zeros_like(yhat_real), tf.ones_like(yhat_fake)], axis=0)
+                
+                # Add some noise to the TRUE outputs
+                noise_real = 0.15*tf.random.uniform(tf.shape(yhat_real))
+                noise_fake = -0.15*tf.random.uniform(tf.shape(yhat_fake))
+                y_realfake += tf.concat([noise_real, noise_fake], axis=0)
+                
+                # Calculate loss - BINARYCROSS 
+                total_d_loss = self.d_loss(y_realfake, yhat_realfake)
+                
+            # Apply backpropagation - nn learn 
+            dgrad = d_tape.gradient(total_d_loss, self.discriminator.trainable_variables) 
+            self.d_opt.apply_gradients(zip(dgrad, self.discriminator.trainable_variables))
+            
+            # Train the generator 
+            with tf.GradientTape() as g_tape: 
+                # Generate some new images
+                gen_images = self.generator(tf.random.normal((100,10,1)), training=True)
+                                            
+                # Create the predicted labels
+                predicted_labels = self.discriminator(gen_images, training=False)
+                                            
+                # Calculate loss - trick to training to fake out the discriminator
+                total_g_loss = self.g_loss(tf.zeros_like(predicted_labels), predicted_labels) 
+                
+            # Apply backprop
+            ggrad = g_tape.gradient(total_g_loss, self.generator.trainable_variables)
+            self.g_opt.apply_gradients(zip(ggrad, self.generator.trainable_variables))
+            
+            return {"d_loss":total_d_loss, "g_loss":total_g_loss}
+    # Create instance of subclassed model
+    fashgan = FashionGAN(generator, discriminator)
+    # Compile the model
+    fashgan.compile(g_opt, d_opt, g_loss, d_loss)
+    import os
+    from tensorflow.keras.preprocessing.image import array_to_img
+    from tensorflow.keras.callbacks import Callback
+    class ModelMonitor(Callback):
+        def __init__(self, num_img=3, latent_dim=10):
+            self.num_img = num_img
+            self.latent_dim = latent_dim
+
+        def on_epoch_end(self, epoch, logs=None):
+            random_latent_vectors = tf.random.uniform((self.num_img, self.latent_dim,1))
+            generated_images = self.model.generator(random_latent_vectors)
+            generated_images *= 255
+            generated_images.numpy()
+            for i in range(self.num_img):
+                img = array_to_img(generated_images[i])
+                img.save(os.path.join('images', f'generated_img_{epoch}_{i}.png'))
+    #
+    #
+    # Recommend 2000 epochs
+    print('ready to train')
+    
+
     hist = fashgan.fit(ds, epochs=200, callbacks=[ModelMonitor()])
     fig = plt.figure(2)
     plt.suptitle('Loss')
