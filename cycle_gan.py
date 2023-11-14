e6b3c60a0086bec228d59db85efb7460859fa830
Paulo_Quintero <33006816+pquinterome@users.noreply.github.com>
2023-11-14 09:31:07 -0500

    cycleGAN

---
+++ cycle_gan.py
@@ -0,0 +1,396 @@
+import os
+import sys
+import pandas as pd
+import matplotlib.pyplot as plt
+from math import sqrt
+from matplotlib.pyplot import figure
+import numpy as np
+import seaborn as sns
+import scipy
+import scipy.io
+import scipy.ndimage
+from scipy.spatial.distance import directed_hausdorff
+import scipy.ndimage
+import random
+import gc
+from skimage import metrics
+from skimage import measure
+from sklearn.metrics import jaccard_score
+from sklearn.metrics import mean_absolute_error as mae
+from skimage.metrics import mean_squared_error as mse
+from skimage.metrics import normalized_root_mse as nrmse
+from skimage.metrics import structural_similarity as ssim
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, KFold, StratifiedKFold
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import numpy as np
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Conv2D, Conv3D, MaxPool2D, MaxPool3D, Flatten, Dropout, GlobalMaxPooling3D, BatchNormalization
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping
+import h5py
+###################
+import tensorflow_datasets as tfds
+from tensorflow_examples.models.pix2pix import pix2pix
+import os
+import time
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+AUTOTUNE = tf.data.AUTOTUNE
+#######################
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#tf.config.list_physical_devices('GPU')
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+######################################
+#import tensorflow as tf
+#gpus = tf.config.experimental.list_physical_devices('GPU')
+#for gpu in gpus: 
+#    tf.config.experimental.set_memory_growth(gpu, True)
+
+# Bringing in matplotlib for viz stuff
+from matplotlib import pyplot as plt
+# Bring in the sequential api for the generator and discriminator
+from tensorflow.keras.models import Sequential
+# Bring in the layers for the neural network
+from tensorflow.keras.layers import Conv2D, Dense, Flatten, Reshape, LeakyReLU, Dropout, UpSampling2D
+
+#########Inputs and Outputs##########
+ct = np.load('inputs/ct2.npy')
+cbct = np.load('inputs/cbct2.npy')
+ct = ct.reshape(3920, 512, 512,1)
+cbct = cbct.reshape(3920, 512, 512,1)
+print('CT shape', ct.shape)
+X = ct
+y = cbct
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, test_size=0.8, random_state=123)
+#
+#
+#
+train_horses= tf.data.Dataset.from_tensor_slices(X_train)
+train_zebras= tf.data.Dataset.from_tensor_slices(y_train)
+test_horses= tf.data.Dataset.from_tensor_slices(X_valid)
+test_zebras= tf.data.Dataset.from_tensor_slices(y_valid)
+#
+BUFFER_SIZE = 100
+BATCH_SIZE = 20
+IMG_HEIGHT=512 
+IMG_WIDTH=512
+
+def random_crop(image):
+  cropped_image = tf.image.random_crop(
+      image, size=[IMG_HEIGHT, IMG_WIDTH, 1])
+
+  return cropped_image
+
+def random_jitter(image):
+  # resizing to 286 x 286 x 3
+  image = tf.image.resize(image, [512, 512], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+  # randomly cropping to 256 x 256 x 3
+  image = random_crop(image)
+  # random mirroring
+  image = tf.image.random_flip_left_right(image)
+  return image
+
+def preprocess_image_train(image):
+  image = random_jitter(image)
+  return image
+
+def preprocess_image_test(image):
+  image = image
+  #image = normalize(image)
+  return image
+#
+train_horses = train_horses.cache().map(
+    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+    BUFFER_SIZE).batch(BATCH_SIZE)
+
+train_zebras = train_zebras.cache().map(
+    preprocess_image_train, num_parallel_calls=AUTOTUNE).shuffle(
+    BUFFER_SIZE).batch(BATCH_SIZE)
+
+test_horses = test_horses.map(
+    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+    BUFFER_SIZE).batch(BATCH_SIZE)
+
+test_zebras = test_zebras.map(
+    preprocess_image_test, num_parallel_calls=AUTOTUNE).cache().shuffle(
+    BUFFER_SIZE).batch(BATCH_SIZE)
+#
+sample_horse = next(iter(train_horses))
+sample_zebra = next(iter(train_zebras))
+#
+#
+def downsample(filters, size, apply_batchnorm=True):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+      tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                             kernel_initializer=initializer, use_bias=False))
+
+  if apply_batchnorm:
+    result.add(tf.keras.layers.BatchNormalization())
+
+  result.add(tf.keras.layers.LeakyReLU())
+
+  return result
+
+def upsample(filters, size, apply_dropout=False):
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  result = tf.keras.Sequential()
+  result.add(
+    tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+  result.add(tf.keras.layers.BatchNormalization())
+
+  if apply_dropout:
+      result.add(tf.keras.layers.Dropout(0.5))
+
+  result.add(tf.keras.layers.ReLU())
+
+  return result
+
+def Generator():
+  inputs = tf.keras.layers.Input(shape=[512, 512, 1])
+
+  down_stack = [
+    downsample(64, 4, apply_batchnorm=False),  # (batch_size, 256, 256, 64)
+    downsample(128, 4),  # (batch_size, 128, 128, 128)
+    downsample(256, 4),  # (batch_size, 64, 64, 256)
+    downsample(512, 4),  # (batch_size, 32, 32, 512)
+    downsample(512, 4),  # (batch_size, 16, 16, 512)
+    downsample(512, 4),  # (batch_size, 8, 8, 512)
+    downsample(512, 4),  # (batch_size, 4, 4, 512)
+    downsample(512, 4),  # (batch_size, 2, 2, 512)
+    downsample(512, 4),  # (batch_size, 1, 1, 512)
+  ]
+
+  up_stack = [
+    upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
+    upsample(512, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
+    upsample(512, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
+    upsample(512, 4, apply_dropout=True),  # (batch_size, 16, 16, 1024)
+    upsample(512, 4),  # (batch_size, 32, 32, 1024)
+    upsample(512, 4),  # (batch_size, 64, 32, 512)
+    upsample(256, 4),  # (batch_size, 128, 3, 512)
+    upsample(128, 4),  # (batch_size, 256, 256, 256)
+    upsample(64, 4),  # (batch_size, 128, 128, 128)
+  ]
+
+  initializer = tf.random_normal_initializer(0., 0.02)
+  last = tf.keras.layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                         strides=2,
+                                         padding='same',
+                                         kernel_initializer=initializer,
+                                         activation='relu')  #tanh (batch_size, 256, 256, 3)
+
+  x = inputs
+
+  # Downsampling through the model
+  skips = []
+  for down in down_stack:
+    x = down(x)
+    skips.append(x)
+
+  skips = reversed(skips[:-1])
+
+  # Upsampling and establishing the skip connections
+  for up, skip in zip(up_stack, skips):
+    x = up(x)
+    x = tf.keras.layers.Concatenate()([x, skip])
+
+  x = last(x)
+
+  return tf.keras.Model(inputs=inputs, outputs=x)
+
+def Discriminator():
+  initializer = tf.random_normal_initializer(0., 0.02)
+
+  inp = tf.keras.layers.Input(shape=[512, 512, 1], name='input_image')
+  tar = tf.keras.layers.Input(shape=[512, 512, 1], name='target_image')
+
+  x = tf.keras.layers.concatenate([inp, tar])  # (batch_size, 256, 256, channels*2)
+
+  
+  down1 = downsample(64, 4, False)(x)  # (batch_size, 128, 128, 64)
+  down2 = downsample(128, 4)(down1)  # (batch_size, 128, 128, 64)
+  down3 = downsample(256, 4)(down2)  # (batch_size, 64, 64, 128)
+  down4 = downsample(512, 4)(down3)  # (batch_size, 32, 32, 256)
+
+  zero_pad1 = tf.keras.layers.ZeroPadding2D()(down4)  # (batch_size, 34, 34, 256)
+  conv = tf.keras.layers.Conv2D(1024, 4, strides=1,
+                                kernel_initializer=initializer,
+                                use_bias=False)(zero_pad1)  # (batch_size, 31, 31, 512)
+
+  batchnorm1 = tf.keras.layers.BatchNormalization()(conv)
+
+  leaky_relu = tf.keras.layers.LeakyReLU()(batchnorm1)
+
+  zero_pad2 = tf.keras.layers.ZeroPadding2D()(leaky_relu)  # (batch_size, 33, 33, 512)
+
+  last = tf.keras.layers.Conv2D(1, 4, strides=1,
+                                kernel_initializer=initializer)(zero_pad2)  # (batch_size, 30, 30, 1)
+
+  return tf.keras.Model(inputs=[inp, tar], outputs=last)
+
+OUTPUT_CHANNELS = 1
+generator_g = Generator()
+generator_f = Generator()
+discriminator_x = Discriminator()
+discriminator_y = Discriminator()
+LAMBDA = 10
+loss_obj = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real, generated):
+  real_loss = loss_obj(tf.ones_like(real), real)
+
+  generated_loss = loss_obj(tf.zeros_like(generated), generated)
+
+  total_disc_loss = real_loss + generated_loss
+
+  return total_disc_loss * 0.5
+
+def generator_loss(generated):
+  return loss_obj(tf.ones_like(generated), generated)
+
+def calc_cycle_loss(real_image, cycled_image):
+  loss1 = tf.reduce_mean(tf.abs(real_image - cycled_image))
+
+  return LAMBDA * loss1
+
+def identity_loss(real_image, same_image):
+  loss = tf.reduce_mean(tf.abs(real_image - same_image))
+  return LAMBDA * 0.5 * loss
+
+generator_g_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+generator_f_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+discriminator_x_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+discriminator_y_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+
+checkpoint_path = "./checkpoints/train"
+
+ckpt = tf.train.Checkpoint(generator_g=generator_g,
+                           generator_f=generator_f,
+                           discriminator_x=discriminator_x,
+                           discriminator_y=discriminator_y,
+                           generator_g_optimizer=generator_g_optimizer,
+                           generator_f_optimizer=generator_f_optimizer,
+                           discriminator_x_optimizer=discriminator_x_optimizer,
+                           discriminator_y_optimizer=discriminator_y_optimizer)
+
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+
+# if a checkpoint exists, restore the latest checkpoint.
+if ckpt_manager.latest_checkpoint:
+  ckpt.restore(ckpt_manager.latest_checkpoint)
+  print ('Latest checkpoint restored!!')
+
+EPOCHS = 100 
+
+def generate_images(model, test_input):
+  prediction = model(test_input)
+
+  plt.figure(figsize=(12, 12))
+
+  display_list = [test_input[0], prediction[0]]
+  title = ['Input Image', 'Predicted Image']
+
+  for i in range(2):
+    plt.subplot(1, 2, i+1)
+    plt.title(title[i])
+    # getting the pixel values between [0, 1] to plot it.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show()
+
+@tf.function
+def train_step(real_x, real_y):
+  # persistent is set to True because the tape is used more than
+  # once to calculate the gradients.
+  with tf.GradientTape(persistent=True) as tape:
+    # Generator G translates X -> Y
+    # Generator F translates Y -> X.
+    fake_y = generator_g(real_x, training=True)
+    cycled_x = generator_f(fake_y, training=True)
+    cycled_x = tf.cast(cycled_x, tf.float64)
+
+    fake_x = generator_f(real_y, training=True)
+    cycled_y = generator_g(fake_x, training=True)
+    cycled_y = tf.cast(cycled_y, tf.float64)
+    # same_x and same_y are used for identity loss.
+    same_x = generator_f(real_x, training=True)
+    same_x = tf.cast(same_x, tf.float64)
+    same_y = generator_g(real_y, training=True)
+    same_y = tf.cast(same_y, tf.float64)
+
+    disc_real_x = discriminator_x((real_x, real_x), training=True)  #or try (real_x, fake_x)
+    disc_real_x  = tf.cast(disc_real_x , tf.float64)
+    disc_real_y = discriminator_y((real_y, real_y), training=True)
+    disc_real_y  = tf.cast(disc_real_y , tf.float64)
+    disc_fake_x = discriminator_x((fake_x, fake_x), training=True)
+    disc_fake_x  = tf.cast(disc_fake_x , tf.float64)
+    disc_fake_y = discriminator_y((fake_y, fake_y), training=True)
+    disc_fake_y  = tf.cast(disc_fake_y , tf.float64)
+    # calculate the loss
+    gen_g_loss = generator_loss(disc_fake_y)
+    gen_g_loss  = tf.cast(gen_g_loss , tf.float64)
+    gen_f_loss = generator_loss(disc_fake_x)
+    gen_f_loss  = tf.cast(gen_f_loss , tf.float64)
+    
+    total_cycle_loss = calc_cycle_loss(real_x, cycled_x) + calc_cycle_loss(real_y, cycled_y)
+
+    # Total generator loss = adversarial loss + cycle loss
+    total_gen_g_loss = gen_g_loss + total_cycle_loss + identity_loss(real_y, same_y)
+    total_gen_f_loss = gen_f_loss + total_cycle_loss + identity_loss(real_x, same_x)
+
+    disc_x_loss = discriminator_loss(disc_real_x, disc_fake_x)
+    disc_y_loss = discriminator_loss(disc_real_y, disc_fake_y)
+
+    # Calculate the gradients for generator and discriminator
+    generator_g_gradients = tape.gradient(total_gen_g_loss, 
+                                            generator_g.trainable_variables)
+    generator_f_gradients = tape.gradient(total_gen_f_loss, 
+                                            generator_f.trainable_variables)
+
+    discriminator_x_gradients = tape.gradient(disc_x_loss, 
+                                                discriminator_x.trainable_variables)
+    discriminator_y_gradients = tape.gradient(disc_y_loss, 
+                                                discriminator_y.trainable_variables)
+
+    # Apply the gradients to the optimizer
+    generator_g_optimizer.apply_gradients(zip(generator_g_gradients, 
+                                                generator_g.trainable_variables))
+
+    generator_f_optimizer.apply_gradients(zip(generator_f_gradients, 
+                                                generator_f.trainable_variables))
+
+    discriminator_x_optimizer.apply_gradients(zip(discriminator_x_gradients,
+                                                    discriminator_x.trainable_variables))
+
+    discriminator_y_optimizer.apply_gradients(zip(discriminator_y_gradients,
+                                                    discriminator_y.trainable_variables))
+
+print('TF dataset', tf.data.Dataset.zip((train_horses, train_zebras)))
+
+n = 0
+for image_x, image_y in tf.data.Dataset.zip((train_horses, train_zebras)):
+  train_step(image_x, image_y)
+  if n % 10 == 0:
+    print ('.', end='')
+  n += 1
+clear_output(wait=True)
+generate_images(generator_g, sample_horse)
+
+plt.savefig('outputs/CycleGAN_generator.png', bbox_inches='tight')
+
+generator_g_optimizer.save('outputs/cycle_g_generator.h5')
+generator_f_optimizer.save('outputs/cycle_f_generator.h5')
+discriminator_x_optimizer.save('outputs/cycle_x_discriminator.h5')
+discriminator_y_optimizer.save('outputs/cycle_y_discriminator.h5')
