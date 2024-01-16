9c17a9799975556a1cdd94160b0a86e6fc44ea23
Paulo_Quintero <33006816+pquinterome@users.noreply.github.com>
2024-01-16 08:07:14 -0500

    try it

--- dibh.py
+++ dibh.py
@@ -69,10 +69,10 @@ def preprocess_image_train(image):
   return image
 
 y_train = tf.data.Dataset.from_tensor_slices(y_trainy)
-y_train = y_train.map(preprocess_image_train).batch(1)
+y_train = y_train.cache().map(preprocess_image_train).batch(1)
 
 y_test = tf.data.Dataset.from_tensor_slices(y_testy)
-y_test = y_test.map(preprocess_image_train).batch(1)
+y_test = y_test.cache().map(preprocess_image_train).batch(1)
 
 
 def read_npy_file(item):
@@ -82,9 +82,9 @@ def read_npy_file(item):
 #file_list = ['/foo/bar.npy', '/foo/baz.npy']
 
 dataset_train = tf.data.Dataset.from_tensor_slices(train)
-dataset_train = dataset_train.map(lambda item:(tf.py_function(read_npy_file, [item], [tf.float64,]))).batch(1)
+dataset_train = dataset_train.cache().map(lambda item:(tf.py_function(read_npy_file, [item], [tf.float64,]))).batch(1)
 dataset_test = tf.data.Dataset.from_tensor_slices(test)
-dataset_test = dataset_test.map(lambda item:(tf.py_function(read_npy_file, [item], [tf.float64,]))).batch(1)
+dataset_test = dataset_test.cache().map(lambda item:(tf.py_function(read_npy_file, [item], [tf.float64,]))).batch(1)
 #dataset = dataset.map(lambda item:tuple(tf.py_function(read_npy_file, [item], [tf.float32,])))
 
 train_dataset = tf.data.Dataset.zip((dataset_train, y_train))
